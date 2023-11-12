8263ff32b7deaff227aaf44177e20dd6d546ec3a
Rikki-Fontaine <146212036+Rikki-Fontaine@users.noreply.github.com>
2023-11-12 15:01:05 -0500

    finihsed basic layout

--- src/components/counter.svelte
+++ src/components/counter.svelte
@@ -5,22 +5,30 @@
     export let imagesrc = "";
     export let bgColor = "";
     export let counterBg = "";
+    export let textColor = "";
+
+    export let count = 0;
 </script>
 
 
 <div class="flex justify-center">
 
-    <div class="border-2 shadow-md shadow-black border-black w-fit p-1 {bgColor} rounded-md">
+    <div class="border-2 shadow-md shadow-black border-black w-fit p-1 {bgColor} rounded-md w-30">
 
         <div class="flex justify-center">
             <img src={imagesrc} alt="pixel heart" width=50 class="shadow-black border-4 bg-orange-300 border-black shadow-inner">
         </div>   
 
         <div class="flex w-full justify-center"> 
-            <div class="flex justify-center mt-5 w-fit">
-                <button class="btn-sm variant-filled-error rounded-sm shadow-[0_1px_2px_4px_rgba(0,0,0,0.0)] shadow-black">-</button>
-                <span class="m-2 text-lg font-[Audiowide] shadow-black {counterBg} p-2 border-2 border-black shadow-inner">20</span>
-                <button class="btn-sm variant-filled-success rounded-sm shadow-[0_1px_2px_4px_rgba(0,0,0,0.0)] shadow-black">+</button>
+            <div class="flex justify-center mt-5 w-30">
+                <button on:click={() => count -=1} class="btn-sm variant-filled-error rounded-sm shadow-[0_1px_2px_4px_rgba(0,0,0,0.0)] shadow-black">-</button>
+                
+                {#if count >= 10 || count < 0}
+                    <span class="m-2 text-left w-12 text-lg font-[Audiowide] shadow-black {counterBg} p-1 border-2 border-black shadow-inner text-center {textColor}">{count}</span>
+                {:else}
+                    <span class="m-2 text-center w-12 text-lg font-[Audiowide] shadow-black {counterBg} p-1 border-2 border-black shadow-inner {textColor}">0{count}</span>
+                {/if}
+                <button on:click={() => count+=1} class="btn-sm variant-filled-success rounded-sm shadow-[0_1px_2px_4px_rgba(0,0,0,0.0)] shadow-black">+</button>
             </div>
         </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,18 @@
 	import medal from "$lib/commdmedal.png"
 </script>
 
-<div class="mt-10 border border-black bg-gradient-to-br">
+<div class="border border-teal-600 rounded-full py-20 md:p-2">
 	<div class="flex justify-center gap-4 mb-5">
-		<Counter bgColor = "bg-secondary-500" imagesrc={heart} counterBg="bg-red-600"/>
-		<Counter bgColor = "bg-primary-600" imagesrc={poison} counterBg="bg-green-600"/>
+		<Counter bgColor = "bg-neutral-300" imagesrc={heart} counterBg="bg-slate-800"/>
+	</div>
+	
+	<div class="flex justify-around mb-5">
+		<Counter bgColor = "bg-success-700" imagesrc={medal} counterBg="bg-red-700"/>
+		<Counter bgColor = "bg-blue-600" imagesrc={poison} counterBg="bg-green-600"/>
 	</div>
 
-	<div class="flex justify-center gap-6">
-		<Counter bgColor = "bg-tertiary-600" imagesrc={medal} counterBg="bg-purple-700"/>
-		<Counter bgColor = "bg-success-800" imagesrc={medal} counterBg="bg-yellow-700"/>
-		<Counter bgColor = "bg-warning-600" imagesrc={medal} counterBg="bg-emerald-700"/>
+	<div class="flex justify-center gap-10 md:gap-40">
+		<Counter bgColor = "bg-red-600" imagesrc={medal} counterBg="bg-blue-600"/>
+		<Counter bgColor = "bg-slate-600" imagesrc={medal} counterBg="bg-neutral-300" textColor="text-black"/>
 	</div>
 </div>
